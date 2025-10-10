2dfff04db1a0efc781ee309235e570c163c5e345
walahow <latifhamzah106@gmail.com>
2025-10-10 19:14:27 +0700

    feat: DLM Arhiving Automation Complete.

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+Schedule::command('dlm:archive-bookings')->weeklyOn(5, '19:10');
+Schedule::command('dlm:delete-archived')->daily();
