dc946c9f8d940056788c6f47b9cf32fe7b94384d
walahow <latifhamzah106@gmail.com>
2025-10-10 19:18:57 +0700

    feat: left somehing out

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,15 +5,26 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\ArchiveOldBookings; // <-- HARUS PAKAI NAMESPACE INI
+use App\Console\Commands\DeleteArchivedData; // <-- DAN NAMESPACE INI
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $commands = [
+        ArchiveOldBookings::class,
+        DeleteArchivedData::class,
+        // Tambahkan command users (dlm:archive-users) di sini jika sudah dibuat
+    ];
     /**
      * Register any application services.
      */
     public function register(): void
     {
         //
+        // 2. Jika aplikasi berjalan di konsol (CLI/Artisan), daftarkan commands
+        if ($this->app->runningInConsole()) {
+            $this->commands($this->commands);
+        }
     }
 
     /**
